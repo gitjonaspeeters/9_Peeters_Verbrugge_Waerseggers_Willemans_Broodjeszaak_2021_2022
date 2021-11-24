5c3c0745347ce76397c43a952ac9b7ce71b8c258
jonathanwillemans <71639359+jonathanwillemans@users.noreply.github.com>
2021-11-24 22:06:09 +0100

    24/11/2021

--- src/view/OrderView.java
+++ src/view/OrderView.java
@@ -19,17 +19,55 @@ import model.database.BroodjesTekstReaderTest;
 import java.util.List;
 
 public class OrderView {
-	private Stage stage = new Stage();		
+	//private Stage stage = new Stage();
+	private TableView<Broodje> table;
+
 		
-	public OrderView(){			
-		stage.setTitle("ORDER VIEW");
-		stage.initStyle(StageStyle.UTILITY);
+	public OrderView(){
+		VBox root = new VBox();
+		root.setSpacing(10);
+		root.setPadding(new Insets(10, 10, 10, 10));
+		Label lblHeading = new Label("Broodjeslijst");
+		lblHeading.setFont(new Font("Arial", 20));
+		table = new TableView<Broodje>();
+		refresh();
+		TableColumn<Broodje, String> colTitle = new TableColumn<Broodje, String>("Soort Broodje");
+		colTitle.setMinWidth(150);
+		colTitle.setCellValueFactory(new PropertyValueFactory<Broodje, String>("name"));
+		TableColumn<Broodje, Double> colPrice = new TableColumn<Broodje, Double>("Prijs");
+		colPrice.setMinWidth(150);
+		colPrice.setCellValueFactory(new PropertyValueFactory<Broodje, Double>("prijs"));
+		TableColumn<Broodje, Integer> colYear = new TableColumn<Broodje, Integer>("Aantal in stock");
+		colYear.setMinWidth(150);
+		colYear.setCellValueFactory(new PropertyValueFactory<Broodje, Integer>("Aantal"));
+		table.getColumns().addAll(colTitle, colYear, colPrice);
+		root.getChildren().addAll(lblHeading, table);
+		Stage stage = new Stage();
+		Scene scene = new Scene(root, 650, 650);
+		stage.setTitle("Broodjes inventory");
 		stage.setX(20);
 		stage.setY(20);
-		Group root = new Group();
-		Scene scene = new Scene(root, 650, 650);			
 		stage.setScene(scene);
-		stage.sizeToScene();			
-		stage.show();		
+
+		stage.show();
 	}
+
+	public void refresh(){
+
+		System.out.println(BroodjesTekstReaderTest.load().values());
+
+		ObservableList<Broodje> broodjes = FXCollections.observableArrayList(BroodjesTekstReaderTest.load().values());
+		table.setItems( broodjes);
+		table.refresh();
+	}
+
+	/*stage.setTitle("ORDER VIEW");
+	stage.initStyle(StageStyle.UTILITY);
+	stage.setX(20);
+	stage.setY(20);
+	Group root = new Group();
+	Scene scene = new Scene(root, 650, 650);
+	stage.setScene(scene);
+	stage.sizeToScene();
+	stage.show();*/
 }
